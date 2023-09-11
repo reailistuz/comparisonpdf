bea14a72edc0647c5812e16a46ff59094664aa2f
reailistuz <reailistuz@gmail.com>
2023-09-11 12:21:11 +0500

    #1 - added project

---
+++ PdfComparison.Console.Aspose/Program.cs
@@ -0,0 +1,43 @@
+﻿using Aspose.Pdf;
+using System.Net;
+
+const string _DataDir = "..\\..\\..\\Samples";
+
+await StaticWeb();
+
+async Task StaticWeb()
+{
+
+	const string url = "https://someurl.org/";
+	HtmlLoadOptions options = new HtmlLoadOptions(url)
+	{
+		PageInfo = { Width = 842, Height = 1191, IsLandscape = false }
+	};
+	var stream = await GetContentFromUrlAsStream(url);
+	Document pdfDocument = new Document(stream, options);
+	pdfDocument.Save("yourName.pdf");
+
+
+}
+
+static async Task<Stream> GetContentFromUrlAsStream(string url, ICredentials credentials = null)
+{
+	using (var httpClient = new HttpClient())
+	{
+		return await httpClient.GetStreamAsync(url);
+	}
+}
+
+void HelloWorld()
+{
+	// Initialize document object
+	Document document = new Document();
+	// Add page
+	Page page = document.Pages.Add();
+	// Add text to new page
+	page.Paragraphs.Add(new Aspose.Pdf.Text.TextFragment("Hello World!"));
+	// Save updated PDF
+	var outputFileName = System.IO.Path.Combine(_DataDir, "HelloWorld_out.pdf");
+	document.Save(outputFileName);
+
+}
---
+++ PdfComparisons.Console.IronPdf/Program.cs
@@ -0,0 +1,57 @@
+﻿using IronPdf;
+using System.Diagnostics;
+
+StaticWeb();
+
+void HelloWorldDemo()
+{
+
+	var Renderer = new IronPdf.ChromePdfRenderer();
+
+	using var pdf = Renderer.RenderHtmlAsPdf("<h1>hello world</h1>");
+
+	pdf.SaveAs("output.pdf");
+
+}
+
+void StaticWeb()
+{
+
+	var sw = new Stopwatch();
+	sw.Start();
+  var Renderer = new IronPdf.ChromePdfRenderer(); 
+	Renderer.RenderingOptions.RenderDelay = 1000;
+	using var pdf = Renderer.RenderUrlAsPdf("https://someurl.org/");
+
+	pdf.SaveAs("kliptokstatic-iron.pdf");
+	Console.WriteLine($"Completed rendering in {sw.ElapsedMilliseconds}ms");
+
+}
+
+void FullWeb()
+{
+
+	var sw = new Stopwatch();
+	sw.Start();
+	var Renderer = new IronPdf.ChromePdfRenderer();
+	Renderer.RenderingOptions.RenderDelay = 1000;
+	using var pdf = Renderer.RenderUrlAsPdf("https://espn.com/");
+
+	pdf.SaveAs("espn-iron.pdf");
+	Console.WriteLine($"Completed rendering in {sw.ElapsedMilliseconds}ms");
+
+}
+
+void FormWeb()
+{
+
+	var sw = new Stopwatch();
+	sw.Start();
+	var Renderer = new IronPdf.ChromePdfRenderer();
+	Renderer.RenderingOptions.RenderDelay = 1000;
+	using var pdf = Renderer.RenderUrlAsPdf("https://localhost:7245/");
+
+	pdf.SaveAs("form-iron.pdf");
+	Console.WriteLine($"Completed rendering in {sw.ElapsedMilliseconds}ms");
+
+}
